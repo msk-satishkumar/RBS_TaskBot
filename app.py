5e2e0da06ad6a8b09b23ec0b68eeaacdd93ba7d8
msk-satishkumar <nandhiseva@gmail.com>
2026-03-27 13:42:13 +0530

    Fix: remove unexpected indentation on line 1 of app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-                                                                                                                                                            import streamlit as st
+import streamlit as st
 import pandas as pd
 from datetime import datetime, date, timedelta
 from streamlit_option_menu import option_menu
